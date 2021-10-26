e50b13f0285e3116f833024d1db812468cf706e6
Melany0213 <90070729+Melany0213@users.noreply.github.com>
2021-10-26 18:47:43 -0400

    Add files via upload

---
+++ Datos/ServiciosSalud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datos
+{
+    public class ServiciosSalud : DbContext
+
+    {
+       public ServiciosSalud( DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Afiliado> Afiliados { get; set; }
+    
+    }
+}
